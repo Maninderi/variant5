c2e1453e3d7688f60df43d885a184f889b1d39c2
Rybalka <rybalkaj@gmail.com>
2024-10-31 03:09:50 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wasd
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void miInput_Click(object sender, RoutedEventArgs e)
+        {
+            if(wrapPanel.Visibility == Visibility.Hidden)
+            {
+                wrapPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                wrapPanel.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработчик: Рыбалка Женя");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
