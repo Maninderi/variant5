63d89780990455aa24c497c17a85a3b05e47e47c
ikmsh <mrsopelka@gmail.com>
2024-10-31 11:47:05 +0500

    Добавил изменения

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -44,7 +44,42 @@ namespace wasd
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            miCalc_Click(sender, e);
+        }
 
+        private void num1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void miCalc_Click(object sender, RoutedEventArgs e)
+        {
+            int a = Convert.ToInt32(num1.Text);
+            int b = Convert.ToInt32(num2.Text);
+            int c = Convert.ToInt32(num3.Text);
+
+            if (c1.IsChecked == true)
+            {
+                int OutPut = a + b + c;
+                MessageBox.Show($"Результат:  {Convert.ToInt32(OutPut)}", "Ответ", MessageBoxButton.OK);
+            } else if (c2.IsChecked == true)
+            {
+                ;
+                
+            }
+            else if(c1.IsChecked == false && c2.IsChecked == false)
+            {
+                MessageBox.Show("Нужно нажать на один из вариантов решения!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            } else if(c1.IsChecked == true && c2.IsChecked == true)
+            {
+                int OutPut = a + b + c;
+                MessageBox.Show($"Сумма:  {Convert.ToInt32(OutPut)}\n Наименьшее кратное: ...", "Ответ", MessageBoxButton.OK);
+            }
+        }
+        
+        private void miExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
