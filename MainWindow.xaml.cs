6d253074115bb0c172c6a2ddb53d4a5d386d331d
Rybalka <rybalkaj@gmail.com>
2024-10-31 23:30:08 +0500

    Доделал

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,6 +25,22 @@ namespace wasd
             InitializeComponent();
         }
 
+        private int FindLCM(int a, int b)
+        {
+            return (a * b) / FindGCD(a, b);
+        }
+
+        private int FindGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
         private void miInput_Click(object sender, RoutedEventArgs e)
         {
             if(wrapPanel.Visibility == Visibility.Hidden)
@@ -42,14 +58,21 @@ namespace wasd
             MessageBox.Show("Разработчик: Рыбалка Женя");
         }
 
+        private bool check()
+        {
+            return int.TryParse(num1.Text, out _) &&
+                   int.TryParse(num2.Text, out _) &&
+                   int.TryParse(num3.Text, out _);
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             miCalc_Click(sender, e);
         }
 
-        private void num1_TextChanged(object sender, TextChangedEventArgs e)
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            Button_Result.IsEnabled = check();
+            miCalc.IsEnabled = check();
         }
 
         private void miCalc_Click(object sender, RoutedEventArgs e)
@@ -58,23 +81,24 @@ namespace wasd
             int b = Convert.ToInt32(num2.Text);
             int c = Convert.ToInt32(num3.Text);
 
-            if (c1.IsChecked == true)
+            int lcm = FindLCM(a, b);
+            int OutPut = a + b + c;
+
+
+            if (c1.IsChecked == true && c2.IsChecked == false)
             {
-                int OutPut = a + b + c;
-                MessageBox.Show($"Результат:  {Convert.ToInt32(OutPut)}", "Ответ", MessageBoxButton.OK);
-            } else if (c2.IsChecked == true)
-            {
-                ;
-                
+                MessageBox.Show($"Сумма:  {Convert.ToInt32(OutPut)}", "Ответ", MessageBoxButton.OK);
             }
-            else if(c1.IsChecked == false && c2.IsChecked == false)
+            else if (c2.IsChecked == true && c1.IsChecked == false)
             {
-                MessageBox.Show("Нужно нажать на один из вариантов решения!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            } else if(c1.IsChecked == true && c2.IsChecked == true)
-            {
-                int OutPut = a + b + c;
-                MessageBox.Show($"Сумма:  {Convert.ToInt32(OutPut)}\n Наименьшее кратное: ...", "Ответ", MessageBoxButton.OK);
+                MessageBox.Show($"Наименьшее кратное: {lcm}", "Ответ", MessageBoxButton.OK);
             }
+            else if (c1.IsChecked == true && c2.IsChecked == true)
+            {
+                MessageBox.Show($"Сумма:  {Convert.ToInt32(OutPut)}\nНаименьшее кратное: {lcm}", "Ответ", MessageBoxButton.OK);
+            }
+            else
+                MessageBox.Show("Нужно нажать на один из вариантов решения!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error); 
         }
         
         private void miExit_Click(object sender, RoutedEventArgs e)
